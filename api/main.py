30ecb3f9bbdf99867d28f45bd67814bdd293630f
Ardyn Sky <jimmydeanbrighton@gmail.com>
2026-09-15 00:18:23 +0100

    Fix Help router line endings

--- api/main.py
+++ api/main.py
@@ -47,7 +47,8 @@ from .agendas import router as agendas_router
 from .live_room_test import apply_authorization_identity, router as live_room_test_router
 from .member_progress import router as member_progress_router
 from .homepage_settings import router as homepage_settings_router
-from .homepage_settings import ensure_homepage_media_optimized\nfrom .help_cms import router as help_cms_router
+from .homepage_settings import ensure_homepage_media_optimized
+from .help_cms import router as help_cms_router
 
 try:
     from dotenv import load_dotenv
@@ -71,7 +72,8 @@ app.include_router(debate_games_router)
 app.include_router(agendas_router)
 app.include_router(live_room_test_router)
 app.include_router(member_progress_router)
-app.include_router(homepage_settings_router)\napp.include_router(help_cms_router)
+app.include_router(homepage_settings_router)
+app.include_router(help_cms_router)
 
 CORS_ALLOWED_ORIGINS = [
     "null",
